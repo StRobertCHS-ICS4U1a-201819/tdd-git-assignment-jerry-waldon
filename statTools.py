a19ed8e40511943f9bf661db6de1be46d6b515d0
Jerry123455 <JiangShan.Cui18@ycdsbk12.ca>
2018-11-11 21:34:56 -0600

    general case 1 commits on mode function

--- statTools.py
+++ statTools.py
@@ -1,1 +1,41 @@
+"""
+-------------------------------------------------------------------------------
+Name:     statTools.py
+Purpose:
+Four programs developed by the TDD method
 
+Author:       Cui.Jerry
+
+Created:      11-11-2018
+------------------------------------------------------------------------------
+"""
+
+def mean(myList):
+   """Determine the average of the values that the list given
+
+   :param myList: A list of random integers
+   :return: the average of the list given
+   """
+
+   # return 0 when the list is empty
+   if len(myList) == 0:
+       return 0
+
+   else:
+       sum = 0
+
+       # apply for loop to compute sum of each value
+       for i in range(len(myList)):
+           sum += myList[i]
+
+       avg = sum / len(myList)     # get average
+       return avg
+
+def mode(myList):
+   """Determine the value that appears with largest frequency in the values in the list given
+
+   :param myList: A list of random integers
+   :return: the value that appears with largest frequency
+   """
+
+   return 2
--- test_statTools.py
+++ test_statTools.py
@@ -1,1 +1,25 @@
+"""
+-------------------------------------------------------------------------------
+Name:		test_statTools.py
+Purpose:
+white-box test and black-box test on the programs in statTools.py
 
+Author:		Cui.Jerry
+
+Created:		11/11/2018
+------------------------------------------------------------------------------
+"""
+
+import pytest
+from statTools import *
+
+def test_mean_basic1():
+   assert(mean([1,2,3]) == 2)
+def test_mean_basic2():
+   assert(mean([1,2,3,4,5]) == 3)
+def test_mean_empty():
+   assert(mean([]) == 0)
+
+
+def test_mode_basic1():
+   assert(mode([2,4,3,2,2,2]) == 2)
