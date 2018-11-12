d842f994316e1f28dc1bb9da4c30c770c18a6484
Jerry123455 <JiangShan.Cui18@ycdsbk12.ca>
2018-11-11 20:40:46 -0600

    general

--- statTools.py
+++ statTools.py
@@ -1,1 +1,27 @@
+"""
+-------------------------------------------------------------------------------
+Name:     statTools.py
+Purpose:
+Four programs developed by the TDD method
 
+Author:       Cui.Jerry
+
+Created:      11-11-2018
+------------------------------------------------------------------------------
+"""
+
+def mean(myList):
+    """Determine the average of the values that the list given
+
+    :param myList: A list of random integers
+    :return: the average of the list given
+    """
+
+    sum = 0
+
+    # apply for loop to compute sum of each value
+    for i in range(len(myList)):
+        sum += myList[i]
+
+    avg = sum / len(myList)  # get average
+    return avg
